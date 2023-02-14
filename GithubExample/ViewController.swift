d94acad595f7da4fc98883bf76c883c3fa9d8155
Student <student@iMac15.local>
2023-02-14 10:07:22 -0600

    first commit

---
+++ GithubExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubExample
+//
+//  Created by Student on 2/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
